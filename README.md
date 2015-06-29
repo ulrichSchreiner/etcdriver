1ae6b066c01c697a9feb6f968735186242803796
Ulrich Schreiner <ulrich.schreiner@gmail.com>
2015-06-29 21:53:56 +0200

    rework to uses different mountpoints for different base trees

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # etcdriver
+
+This is a docker volume plugin for etcd. It uses [etcdfs](github.com/xetorthio/etcd-fs/src/etcdfs)
+to mount a tree from etcd to a temporary path and passes this local path to docker. Inside your
+container you can use normal filesystem functions to access this etcd subtree.
+
+So you can have a etcd cluster and multiple docker containers which have their
+configs in etcd stored without using the etcd rest API.
+
+Well, mostly this is only a test plugin for me to learn the docker plugin API :-)
--- driver.go
+++ driver.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
-	"path/filepath"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/hanwen/go-fuse/fuse"
 	"github.com/hanwen/go-fuse/fuse/nodefs"
@@ -14,11 +13,24 @@ import (
 	"github.com/xetorthio/etcd-fs/src/etcdfs"
 )
 
-var etcdbackend etcdfs.EtcdFs
-var mountpoints map[string]*etcdFuseServer
-var mux sync.Mutex
+var (
+	etcdbackend  etcdfs.EtcdFs
+	mountpoints  map[string]*etcdFuseServer
+	mux          sync.Mutex
+	basePath     string
+	etcdEndpoint string
+)
 
-const basePath = "/tmp/etcd"
+func init() {
+	basePath = os.Getenv("ETCDRIVER_BASE")
+	if basePath == "" {
+		basePath = "/tmp/etcd"
+	}
+	etcdEndpoint = os.Getenv("ETCD_ENDPOINT")
+	if etcdEndpoint == "" {
+		etcdEndpoint = "http://localhost:4001"
+	}
+}
 
 type etcdFuseServer struct {
 	mountpoint string
@@ -27,7 +39,7 @@ type etcdFuseServer struct {
 }
 
 func createPath(p string) string {
-	return filepath.Join(basePath, asEtcdRoot(p))
+	return fmt.Sprintf("%s/%d", basePath, time.Now().Nanosecond())
 }
 
 func asEtcdRoot(r string) string {
@@ -35,21 +47,56 @@ func asEtcdRoot(r string) string {
 }
 
 func activate() []string {
-	etcdbackend = etcdfs.EtcdFs{FileSystem: pathfs.NewDefaultFileSystem(), EtcdEndpoint: "http://localhost:4001"}
+	etcdbackend = etcdfs.EtcdFs{FileSystem: pathfs.NewDefaultFileSystem(), EtcdEndpoint: etcdEndpoint}
 	mountpoints = make(map[string]*etcdFuseServer)
 	return []string{"VolumeDriver"}
 }
 
 func create(volume string) error {
-	log.Printf("create %s", volume)
 	if !strings.HasPrefix(volume, "@") {
 		return fmt.Errorf("a etcd path has to start with a @")
 	}
+	mux.Lock()
+	defer mux.Unlock()
+
+	_, ok := mountpoints[volume]
+	if ok {
+		return nil
+	}
+
+	pt := createPath(volume)
+	if err := os.MkdirAll(pt, 0755); err != nil {
+		return fmt.Errorf("cannot mkdir: %s", err)
+	}
+	rootfs := "/" + asEtcdRoot(volume)
+	root := pathfs.NewPrefixFileSystem(&etcdbackend, rootfs)
+	nfs := pathfs.NewPathNodeFs(root, nil)
+	server, _, err := nodefs.MountRoot(pt, nfs.Root(), nil)
+
+	if err != nil {
+		return fmt.Errorf("cannot mount root : %s", err)
+	}
+	es := etcdFuseServer{pt, server, 0}
+	mountpoints[volume] = &es
+
+	go server.Serve()
 	return nil
 }
 
 func remove(volume string) error {
-	log.Printf("remove %s", volume)
+	mux.Lock()
+	defer mux.Unlock()
+
+	s, ok := mountpoints[volume]
+	if ok {
+		if s.count == 0 {
+			delete(mountpoints, volume)
+			if e := s.server.Unmount(); e != nil {
+				return e
+			}
+			return os.Remove(s.mountpoint)
+		}
+	}
 	return nil
 }
 
@@ -63,45 +110,28 @@ func mount(volume string) (string, error) {
 		return s.mountpoint, nil
 	}
 
-	pt := createPath(volume)
-	if err := os.MkdirAll(pt, 0755); err != nil && !os.IsExist(err) {
-		return "", fmt.Errorf("cannot mk dir: %s", err)
-	}
-	rootfs := "/" + asEtcdRoot(volume)
-	root := pathfs.NewPrefixFileSystem(&etcdbackend, rootfs)
-	nfs := pathfs.NewPathNodeFs(root, nil)
-	server, _, err := nodefs.MountRoot(pt, nfs.Root(), nil)
-
-	//nfs := pathfs.NewPathNodeFs(&etcdbackend, nil)
-	//log.Printf("mounting to: %s", pt)
-	//server, _, err := nodefs.MountRoot(pt, nfs.Root(), nil)
-	if err != nil {
-		return "", fmt.Errorf("cannot mount root : %s", err)
-	}
-	es := etcdFuseServer{pt, server, 0}
-	mountpoints[volume] = &es
-
-	go server.Serve()
-	return pt, nil
+	return "", fmt.Errorf("%s not created", volume)
 }
 
 func path(volume string) (string, error) {
-	return createPath(volume), nil
+	mux.Lock()
+	defer mux.Unlock()
+
+	s, ok := mountpoints[volume]
+	if ok {
+		return s.mountpoint, nil
+	}
+
+	return "", fmt.Errorf("%s not found", volume)
 }
 
 func unmount(volume string) error {
 	mux.Lock()
 	defer mux.Unlock()
 
-	v := asEtcdRoot(volume)
-	s, ok := mountpoints[v]
+	s, ok := mountpoints[volume]
 	if ok {
-		if s.count == 0 {
-			delete(mountpoints, v)
-			return s.server.Unmount()
-		} else {
-			s.count = s.count - 1
-		}
+		s.count = s.count - 1
 	}
 	return nil
 }
